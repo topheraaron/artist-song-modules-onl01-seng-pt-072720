69e03666d0171f987ec706b68fba5422b9057863
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-28 23:02:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,8 +3,8 @@ require 'pry'
 class Artist
 
   extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
-  
+  #include Memorable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
