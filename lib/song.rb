ad1a2738a5759c3c96d9ea600fc8170ba7a14ef3
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-28 23:52:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,10 +16,6 @@ class Song
     @@songs << self
   end
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
-
   def self.all
     @@songs
   end
@@ -27,8 +23,4 @@ class Song
   def artist=(artist)
     @artist = artist
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
