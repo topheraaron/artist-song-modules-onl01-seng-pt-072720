b5d0e0e277dc6cb77075c2f9cf6b8f22a2ec8fa0
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-28 23:20:39 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,12 +3,12 @@ require 'pry'
 module Memorable
 
   module ClassMethods
-    
-    def self.reset_all
+
+    def reset_all
       self.all.clear
     end
 
-    def self.count
+    def count
       self.all.count
     end
   end
